cec0aed8785b3820785e8a7d415b8869b68b93db
JxAces <jadeaceflor@gmail.com>
2023-08-09 00:03:31 +0800

    seach-bar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,56 +1,59 @@
 @extends('layouts.user_type.auth')
 
 @section('content')
-    <!-- Add the search bar HTML here -->
-    <div class="search-container">
-        <input type="text" id="searchInput" class="search-input" placeholder="Search students" oninput="searchStudents()">
-        <div class="search-results" id="searchResults">
-            <!-- Display search results here -->
+
+<div class="d-flex justify-content-center align-items-center">
+    <div class="col-12 col-md-6">
+        <div class="card mb-4">
+            <div class="card-header pb-0">
+                <div class="search-container">
+                    <input
+                        type="text"
+                        id="searchInput"
+                        class="form-control"
+                        placeholder="Search students"
+                        oninput="searchStudents()"
+                    >
+                    <div class="search-results" id="searchResults"></div>
+                </div>
+            </div>
+            <div class="card-body px-2 pt-3 pb-2">
+                <!-- Your card body content goes here -->
+            </div>
         </div>
     </div>
+</div>
+
 @endsection
 
 @push('dashboard')
 <style>
+    /* Add your custom styling here */
     .search-container {
         position: relative;
-        width: 100%;
-        max-width: 400px;
-    }
-
-    .search-input {
-        width: 100%;
-        padding: 10px;
-        font-size: 16px;
+        text-align: center;
     }
 
     .search-results {
         position: absolute;
-        top: 100%;
-        left: 0;
-        z-index: 100;
+        background-color: white;
+        border: 1px solid #ccc;
+        box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
         width: 100%;
-        max-height: 200px; /* Set a fixed height for the search results container */
-        overflow-y: auto; /* Enable vertical scrolling for the search results */
-        background-color: #fff;
-        border: 1px solid #ddd;
-        border-radius: 4px;
-        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        max-width: 600px;
+        max-height: 400px;
+        overflow-y: auto;
         display: none;
+        margin-top: 2px; /* Add margin-top to separate search bar and results */
     }
 
     .search-results div {
         padding: 10px;
-        border-bottom: 1px solid #ddd;
         cursor: pointer;
     }
 
-    .search-results div:last-child {
-        border-bottom: none;
-    }
-
     .search-results div:hover {
-        background-color: #f9f9f9;
+        background-color: #f5f5f5;
     }
 </style>
 
@@ -70,12 +73,16 @@
                 if (data.length > 0) {
                     data.forEach(student => {
                         var resultItem = document.createElement('div');
-                        resultItem.textContent = student.full_name; // Assuming 'full_name' is the correct property to display the student's name
+                        resultItem.innerHTML = `<strong>${student.id_no}</strong> - ${student.full_name}, Major: ${student.major}`;
+                        resultItem.addEventListener('click', () => {
+                            // Handle click on result item
+                            // Example: window.location.href = '/students/' + student.id;
+                        });
                         resultsDiv.appendChild(resultItem);
                     });
-                    resultsDiv.style.display = 'block'; // Show the search results container
+                    resultsDiv.style.display = 'block';
                 } else {
-                    resultsDiv.style.display = 'none'; // Hide the search results container if no results found
+                    resultsDiv.style.display = 'none';
                 }
             })
             .catch(error => {
