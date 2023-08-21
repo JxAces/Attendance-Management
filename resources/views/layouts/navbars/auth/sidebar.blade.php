1ab7e307bb306e678355f80295dd94ee8e2a2418
JxAces <jadeaceflor@gmail.com>
2023-08-21 21:59:27 +0800

    login design

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -113,6 +113,15 @@
           <span class="nav-link-text ms-1">Events</span>
         </a>
       </li>
+      <li class="nav-item">
+        <a class="nav-link {{ (Request::is('student.search') ? 'active' : '') }}" href="{{ url('/logout') }}">
+        <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+
+            <i class="fas fa-globe" style="font-size: 18px;"></i>
+            </div>
+          <span class="nav-link-text ms-1">Log Out</span>
+        </a>
+      </li>
     </ul>
   @endif
   </div>
