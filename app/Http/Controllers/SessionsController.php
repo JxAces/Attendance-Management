e85710e2c9c18bc311a5b556e6d2b37725bd039f
hannah.hontiveros <hannah.hontiveros@g.msuiit.edu.ph>
2025-01-23 10:11:55 +0800

    mobile design

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -23,7 +23,7 @@ class SessionsController extends Controller
         if(Auth::attempt($attributes))
         {
             session()->regenerate();
-            return redirect('dashboard');
+            return redirect('/');
         }
         else{
 
